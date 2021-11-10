db71d92474ba420d0486cad6ba4e823bcbd0cf4a
Miguel C <miguel.c@thenetcircle.com>
2021-11-10 11:37:47 +0100

    Updated custom comment rule to work for CREATE VIEW too

--- plugins/sqlfluff-plugin-comments/src/comments/rules.py
+++ plugins/sqlfluff-plugin-comments/src/comments/rules.py
@@ -39,10 +39,10 @@ def get_configs_info() -> dict:
 @document_fix_compatible
 @document_configuration
 class Rule_Comments_L001(BaseRule):
-    """CREATE TABLE statements should be preceded by a comment section
+    """CREATE TABLE|VIEW statements should be preceded by a comment section
 
     | **Anti-pattern**
-    | Use CREATE TABLE without any documentation about the table .
+    | Use CREATE TABLE|VIEW without any documentation about the table .
 
     .. code-block:: sql
 
@@ -53,7 +53,7 @@ class Rule_Comments_L001(BaseRule):
         HAVING MAX(b) > 10
 
     | **Best practice**
-    | Write a comment right before creating the table
+    | Write a comment right before creating the table|view
 
     .. code-block:: sql
         --
@@ -71,8 +71,8 @@ class Rule_Comments_L001(BaseRule):
         super().__init__(*args, **kwargs)
 
     def _eval(self, context: RuleContext) -> Optional[List[LintResult]]:
-        """We should use comments before CREATE TABLE."""
-        if context.segment.is_type("create_table_statement"):
+        """We should use comments before CREATE TABLE|VIEW."""
+        if context.segment.is_type("create_table_statement") or context.segment.is_type("create_view_statement"):
             seg = context.segment
             for seg in context.raw_stack[-2:]:
                 if seg.name in ['inline_comment', 'block_comment']:
@@ -82,7 +82,7 @@ class Rule_Comments_L001(BaseRule):
                 LintResult(
                     anchor=seg,
                     description=(
-                        "Found CREATE TABLE statement without a comment preceding it."
+                        "Found CREATE TABLE|VIEW statement without a comment preceding it."
                     ),
                 ),
             ]
